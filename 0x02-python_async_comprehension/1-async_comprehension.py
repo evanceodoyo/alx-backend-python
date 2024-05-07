409be6649cd41140db9320bb822cf8f98d241cf5
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-08 01:23:04 +0300

    async comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Async generation and comprehension of random numbers."""
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Asynchronously collects 10 random numbers using async comprehension.
+
+    Returns:
+        List[float]: A list of 10 random floating-point numbers.
+    """
+    return [i async for i in async_generator()][:10]
