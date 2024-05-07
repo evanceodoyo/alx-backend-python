ef857eac4ed1a413bdc602d08136c8fa3165fc66
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-07 06:05:11 +0300

    update function and make it executable

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-'''Task 4's module.
-'''
+"""Executes executive concurrent tasks with random delays."""
+
 import asyncio
 from typing import List
 
@@ -9,9 +9,14 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes task_wait_random n times.
-    '''
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    """Execute task_wait_random concurrently n times.
+
+    Parameters:
+        n (int): The number of times to execute task_wait_random.
+        max_delay (int): The maximum delay time in seconds for each execution.
+
+    Returns:
+        List[float]: A sorted list of the times taken for each execution.
+    """
+    res = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
+    return sorted(res)
