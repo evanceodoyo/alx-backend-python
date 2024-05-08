b0905b8fbc9fe3de8befcb4d1a1dea1a7ada591b
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-08 23:28:55 +0300

    first commit for basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Demonstrate annotated function and arguments"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floating-point numbers.
+
+    Parameters:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Annotated function to cancatenate a two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns result of concatenating two strings.
+
+    Parameters:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        The concatenated string.
+    """
+    return str1 + str2
