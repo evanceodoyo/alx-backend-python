e86f5c5fcaf271c67e66df7aac8d36c5d4b4a2d1
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-09 00:21:39 +0300

    annotating mixed types

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Annotates mixed type - string and int/float"""
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Convert a 'key-value' pair into a tuple where the value is squared.
+
+    Parameters:
+        k (str): The key.
+        v (Union[int, float]): The value, which can be an integer or a
+        floating-point number.
+
+    Returns:
+        Tuple[str, float]: A tuple containing the key and the squared value.
+    """
+    return tuple([k, v ** 2])
